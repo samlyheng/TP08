91cf44897b06a4e0fa62dcf5dc2f7617eb434208
samlyheng <samlyheng@samlyhengs-MacBook-Pro.local>
2022-03-13 02:14:47 +0700

    Done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import TheWelcome from "@/components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
-    <div>
-        <h1> Home page </h1>
-    </div>
+    <div><h1>You're in Home Page</h1></div>
+    <p>Well done🥰</p>
+   <a href="http://localhost:3000"> <button>Back to Login</button></a>
   </main>
 </template>
